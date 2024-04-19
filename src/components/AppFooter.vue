cd26f410d4db3e16061b3661a8254dfc3820e80a
Amine-Triki <amine.triki15@gmail.com>
2024-04-19 22:42:02 +0100

    update footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,8 @@
 <template>
     <footer class=" bg-gray-800 text-gray-300 p-5  ">
-        <p id="Rights" class="container mx-auto text-center "> </p>
+        <p id="Rights" class="container mx-auto text-center ">
+            {{ year }} &copy; Amine Triki || All Rights Reserved    
+        </p>
     </footer>
     
 </template>
@@ -9,10 +11,11 @@
 <script>
 export default {
     name : 'AppFooter' ,
-    mounted(){
-    let year = new Date().getFullYear();
-document.getElementById("Rights").innerHTML = `${year} &copy Amine Triki || All Rights Reserved`;
-}
+    data() {
+      return {
+        year: new Date().getFullYear()
+      }
+    }
 }
 
 </script>
